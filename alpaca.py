93a054e771aea17cd28001797fd218dcbee63c1e
fernandopalafox <fernandopalafox@utexas.edu>
2024-10-25 20:09:20 -0500

    formatting

--- alpaca.py
+++ alpaca.py
@@ -7,10 +7,11 @@ Implementation of ALPaCA as described in https://arxiv.org/abs/1807.08912
 Notation is consistent with the paper
 """
 
+
 class ALPaCA(nn.Module):
-    phi: nn.Module 
+    phi: nn.Module
     Sigma_eps: jnp.ndarray
-    n_y: int 
+    n_y: int
     n_x: int
     n_phi: int
 
@@ -25,13 +26,10 @@ class ALPaCA(nn.Module):
     def __call__(self, x: jnp.ndarray):
         """Applies feature mapping and returns the model's prediction."""
         phi_x = self.phi(x)
-        return (self.Kbar_0.T[None,:,:] @ phi_x[...,None]).squeeze(-1)
+        return (self.Kbar_0.T[None, :, :] @ phi_x[..., None]).squeeze(-1)
 
     def loss_offline(
-        self,
-        params: dict,
-        D: tuple[jnp.ndarray, jnp.ndarray],
-        rng_key: jax.Array
+        self, params: dict, D: tuple[jnp.ndarray, jnp.ndarray], rng_key: jax.Array
     ) -> jnp.ndarray:
         """
         Offline training of the ALPaCA algorithm.
@@ -60,28 +58,28 @@ class ALPaCA(nn.Module):
         # TODO: figure out if we can pad the data to a fixed length
         for j in range(J):
             # sample data for current index
-            Dx_j = Dxs[j, :t_js[j], :]  # (t_j, n_x)
-            Y_j = Dys[j, :t_js[j], :]    # (t_j, n_y)
-            x_jp1 = Dxs[j, t_js[j], :]   # (n_x)
-            y_jp1 = Dys[j, t_js[j], :]   # (n_y)
+            Dx_j = Dxs[j, : t_js[j], :]  # (t_j, n_x)
+            Y_j = Dys[j, : t_js[j], :]  # (t_j, n_y)
+            x_jp1 = Dxs[j, t_js[j], :]  # (n_x)
+            y_jp1 = Dys[j, t_js[j], :]  # (n_y)
 
             # compute NN features
             Phi_j = self.phi.apply(params, Dx_j)  # (t_j, n_phi)
-            phi_j = self.phi.apply(params, x_jp1)  # (n_phi)
+            phi_jp1 = self.phi.apply(params, x_jp1)  # (n_phi)
 
             # compute Lambda_j and Sigma_j using stable solve instead of inv
             Lambda_0 = L0 @ L0.T
             Lambda_j = Phi_j.T @ Phi_j + Lambda_0
-            Lambda_j_inv_phi = jnp.linalg.solve(Lambda_j, phi_j)  # avoid inversion
-            Sigma_j = (1 + phi_j.T @ Lambda_j_inv_phi) * self.Sigma_eps
+            Lambda_j_inv_phi = jnp.linalg.solve(Lambda_j, phi_jp1)  # avoid inversion
+            Sigma_j = (1 + phi_jp1.T @ Lambda_j_inv_phi) * self.Sigma_eps
 
             # compute Kbar_j
             Kbar_j = jnp.linalg.solve(Lambda_j, Phi_j.T @ Y_j + Lambda_0 @ Kbar_0)
 
             # loss
-            y_delta = y_jp1 - Kbar_j.T @ phi_j
+            y_delta = y_jp1 - Kbar_j.T @ phi_jp1
             loss += (
-                self.n_y * jnp.log(1 + phi_j.T @ Lambda_j_inv_phi)
+                self.n_y * jnp.log(1 + phi_jp1.T @ Lambda_j_inv_phi)
                 + y_delta.T @ jnp.linalg.solve(Sigma_j, y_delta)
             ) / J
 
