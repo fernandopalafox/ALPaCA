08cf7c8065b69ed56c42feacf5bb6e8fb9f40902
fernandopalafox <fernandopalafox@utexas.edu>
2024-10-29 13:59:33 -0500

    match paper

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 # Generate data
 M = 1  # Total number of trajectories
-tau = 50  # Number of time steps per trajectory
+tau = 20  # Number of time steps per trajectory
 amplitude_range = (0.1, 5.0)
 phase_range = (0.0, jnp.pi)
 time_range = (-5, 5)
@@ -22,10 +22,10 @@ Dy = Dy.squeeze(0)
 # Load the model
 n_x = Dx.shape[-1]  # Input dimension (should be 1)
 n_y = Dy.shape[-1]  # Output dimension (should be 1)
-n_phi = 16  # Dimension of feature mapping output
+n_phi = 32  # Dimension of feature mapping output
 
 # Define noise covariance
-Sigma_eps = jnp.eye(n_y) * 0.05  # Assumed noise covariance
+Sigma_eps = jnp.eye(n_y) * 0.02  # Assumed noise covariance
 
 # Instantiate the feature mapping module
 phi = DefaultFeatureMapping(n_phi=n_phi)
